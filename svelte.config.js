bc68731da235b469d70138cd977cabf9472acae9
Gataitai <mpattipeilohy1@gmail.com>
2026-05-31 18:03:24 +0800

    fixed svelte.config.js for github pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,19 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const dev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
+
 	compilerOptions: {
-		// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
-		runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
+		runes: ({ filename }) =>
+			filename.split(/[/\\]/).includes('node_modules')
+				? undefined
+				: true
 	},
+
 	kit: {
 		adapter: adapter({
 			pages: 'build',
